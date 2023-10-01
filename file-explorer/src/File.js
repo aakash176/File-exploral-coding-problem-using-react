eb6bec2343894504bb9914317d05eb30acc46a20
aakas176 <aakaas.c.4508@gmail.com>
2023-10-01 11:04:24 +0000

    added functionality for adding folders and files

--- file-explorer/src/File.js
+++ file-explorer/src/File.js
@@ -1,27 +1,61 @@
 import {useState} from 'react'
+import {BsFolderPlus} from 'react-icons/bs'
+import {AiOutlineFileAdd} from 'react-icons/ai'
+
 const File = ({data}) =>{
+    const [add, setAdd] = useState({
+        showInput:false,
+        isFolder:null
+    })
+    const handlePropagation = (e, isFolder) => {
+        e.stopPropagation()
+        setAdd({
+            showInput:true,
+            isFolder
+        })
+    }
+    const [click, setClick] = useState(false)
     const handleClick = () => {
         setClick(!click)
     }
-    const [click, setClick] = useState(false)
     return (
         <div style={{marginLeft:"20px"}}>
             {
                 data.isFolder?(
                     <>
-                    <h3 onClick={handleClick} style={{cursor:'pointer'}} >📁{data.name}</h3>
+                    <h3 onClick={handleClick} style={{cursor:'pointer'}} ><span>📁{data.name}</span>
+                    <button onClick={(e) => handlePropagation(e,true)} style={{margin:'10px', padding:'5px', cursor:'pointer'}} ><BsFolderPlus/></button>
+                    <button onClick ={(e) => handlePropagation(e,false)} style={{margin:'10px', padding:'5px', cursor:'pointer'}} ><AiOutlineFileAdd/></button></h3>
                     {
-                        <div style={{display:click?'':'none'}} >
-                            {
-                            data.items.map(element => {
-                                return (
-                                    <File data={element}/>
+                        
+                        
+                        
+                            add.showInput && (
+                            <div>
+                                <span>
+                                    {
+                                        add.isFolder?'📁':'📄'
+                                    }
+                                </span> 
+                                <input type='text' autoFocus onBlur={()=>setAdd({...add, showInput:false})} />
+                            </div>
+                            )
 
-                                )
-                            })
-                            }
-                        </div>
+                        
+                        
                     }
+                    <div style={{display:click?'':'none'}} >
+                        
+                        {
+                        data.items.map(element => {
+                            return (
+                                <File data={element}/>
+
+                            )
+                        })
+                        }
+                    </div>
+                    
                     </>
 
                 ):
